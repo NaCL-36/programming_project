e5962ce3ca728ce90c3d310e8d1c9491620d883e
NaCL-36 <salemsayed407@gmail.com>
2026-03-31 09:07:37 +0200

    added error explainer feature

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from errors_data import errors
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World 🔥"
+    return render_template("index.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    user_error = request.form.get("error")
+
+    if user_error in errors:
+        data = errors[user_error]
+    else:
+        data = {
+            "description": "Error not found.",
+            "solution": "Try another error.",
+            "example": ""
+        }
+
+    return render_template("result.html", error=user_error, data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
